cf729b98c7db5d4bf1a77dcd9ee2faf340851fc3
Markus Lehtonen <markus.lehtonen@linux.intel.com>
2013-03-23 09:53:14 +0200

    Use python distutils
    Change-Id: I3172b0abadb90e079494b5b1607511e1509c88f8
    Signed-off-by: Markus Lehtonen <markus.lehtonen@linux.intel.com>

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# vim:fileencoding=utf-8:et:ts=4:sw=4:sts=4
+#
+# Copyright (C) 2013 Intel Corporation <markus.lehtonen@linux.intel.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+"""Setup script"""
+
+from setuptools import setup
+
+setup(name='obs_service_gbp',
+      version='0.0.1',
+      description='OBS source service utilizing git-buildpackage',
+      author='Markus Lehtonen',
+      author_email='markus.lehtonen@linux.intel.com',
+      packages=['obs_service_gbp'],
+      data_files=[('/usr/lib/obs/service', ['service/git-buildpackage',
+                                            'service/git-buildpackage.service'])],
+     )
